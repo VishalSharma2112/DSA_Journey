102d9d69415393ca0b894c47520eeaf3bfbb601d
Vishal Sharma <vs25052006@gmail.com>
2026-07-06 11:41:07 +0530

    Time: 16 ms (24.72%), Space: 19.3 MB (75.11%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        maps = {}
+        mapt = {}
+        for i in s:
+            maps[i] = maps.get(i, 0) + 1
+        for i in t:
+            mapt[i] = mapt.get(i, 0) + 1
+
+        return maps==mapt
