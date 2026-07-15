52db713886410591096c84d5098d12f2d69bfa72
Vishal Sharma <vs25052006@gmail.com>
2026-07-15 18:06:37 +0530

    Time: 7 ms (28.62%), Space: 19.4 MB (9.51%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        answer = True
+        maps = {}
+        boxes = {}
+        for i in range(1, 10):
+            maps[i] = {}
+        for i in range(1, 10):
+            boxes[i] = {}
+        for row, i in enumerate(board, start=1):
+            row_check = [0]*9
+            for column, j in enumerate(i, start=1):
+                if j != ".":
+                    num = int(j)
+                    if row_check[num-1] == 0:
+                        row_check[num-1] = 1
+                    else:
+                        answer = False
+                        break
+                    if num not in maps[column]:
+                        maps[column][num] = row
+                    else:
+                        answer = False
+                        break
+                    box = ((row - 1) // 3) * 3 + ((column - 1) // 3) + 1
+
+                    if num in boxes[box]:
+                        answer = False
+                        break
+                    boxes[box][num] = (row, column)
+        return answer
