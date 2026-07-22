bad286887bb14fa4cd63fb231b078ad73fcab38a
Vishal Sharma <vs25052006@gmail.com>
2026-07-22 17:00:25 +0530

    Time: 0 ms (100%), Space: 19.4 MB (22.71%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        alpha_freq = {}
+
+        for i in t:
+            alpha_freq[i] = alpha_freq.get(i, 0)+1
+        for i in s:
+            alpha_freq[i] = alpha_freq.get(i, 0)+1
+        for i in alpha_freq:
+            if alpha_freq[i]%2 != 0:
+                return i
