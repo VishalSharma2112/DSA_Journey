c1f9694e55f08a52ea9daf02dbb79a9de76bc73c
Vishal Sharma <vs25052006@gmail.com>
2026-07-23 16:48:56 +0530

    Time: 0 ms (100%), Space: 19.3 MB (38.36%) - LeetHub

--- 0890-find-and-replace-pattern/0890-find-and-replace-pattern.py
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.py
@@ -1,26 +1,21 @@
 class Solution:
     def findAndReplacePattern(self, words: List[str], pattern: str) -> List[str]:
         answer = []
-        pattern_map = {}
-        num_pattern = 1
-        for i in pattern:
-            if i not in pattern_map:
-                pattern_map[i] = num_pattern
-                num_pattern += 1
-        numerical_pattern = 0
-        for i in pattern:
-            numerical_pattern = numerical_pattern*10 + pattern_map[i]
 
         for word in words:
-            temp = {}
-            index = 1
-            for alpha in word:
-                if alpha not in temp:
-                    temp[alpha] = index
-                    index += 1
-            temp_num_pattern = 0
-            for alpha in word:
-                temp_num_pattern = temp_num_pattern*10 + temp[alpha]
-            if temp_num_pattern == numerical_pattern:
+            W2P = {}
+            P2W = {}
+            result = True
+            for w, p in zip(word, pattern):
+                if w in W2P and W2P[w] != p:
+                    result = False
+                    break
+                if p in P2W and P2W[p] != w:
+                    result = False
+                    break
+                W2P[w] = p
+                P2W[p] = w
+            if result:
                 answer.append(word)
         return answer
+                    
