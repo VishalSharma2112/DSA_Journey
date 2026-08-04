2fa608f2aff856d2a438c61c869ef5ce3d02bf5b
Vishal Sharma <vs25052006@gmail.com>
2026-08-04 22:36:43 +0530

    Time: 2 ms (75.62%), Space: 20.8 MB (66.18%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        XOR = 0
+        for i in nums:
+            XOR ^= i
+        bit_mask = XOR & -XOR
+        XOR1 = 0
+        XOR2 = 0
+        for i in nums:
+            if i & bit_mask == bit_mask:
+                XOR1 ^= i
+            else:
+                XOR2 ^= i
+        return [XOR1, XOR2]
